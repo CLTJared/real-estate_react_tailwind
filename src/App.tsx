c28319d2afb0baa430ce8770378c225fce1ee478
CLTJared <projects@jaredclt.com>
2025-01-08 17:22:40 -0500

    Added state, import propertyProps

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import Footer from "./components/Footer.js";
 import FeaturedProperty from "./components/FeaturedProperty.js";
 import PropertyModel from "./components/PropertyModel.js";
 import Home from "./components/Home.js";
+import { useState } from "react";
+import { propertyProps } from "./components/FeaturedProperty.js"
 
 function App() {
+  const [selectedProperty, setSelectedProperty] = useState<propertyProps|null>(null);
 
   return (
     <div className="min-h-screen w-full bg-gray-50">
       <Home />
-      <FeaturedProperty />
+      <FeaturedProperty setSelectedProperty={setSelectedProperty} />
       <Contact />
       <Footer />
     </div>
