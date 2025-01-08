da71b0c7cff7568440f74ca93b6d66a75c384f35
CLTJared <projects@jaredclt.com>
2025-01-08 12:43:05 -0500

    Add components and base styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Contact from "./components/Contact.js";
 import Footer from "./components/Footer.js";
 import FeaturedProperty from "./components/FeaturedProperty.js";
-import Home from "./components/Home.js";
 import PropertyModel from "./components/PropertyModel.js";
+import Home from "./components/Home.js";
 
 function App() {
 
   return (
-    <>
+    <div className="min-h-screen w-full bg-gray-50">
       <Home />
-    </>
+      <FeaturedProperty />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
