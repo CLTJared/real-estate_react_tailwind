f7fe0929870a078ff6561df5ffa0b0e7019598e2
CLTJared <projects@jaredclt.com>
2025-01-08 12:27:42 -0500

    Changed from jsx to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Contact from "./components/Contact.js";
+import Footer from "./components/Footer.js";
+import FeaturedProperty from "./components/FeaturedProperty.js";
+import Home from "./components/Home.js";
+import PropertyModel from "./components/PropertyModel.js";
 
 function App() {
 
   return (
     <>
-
+      <Home />
     </>
   )
 }
